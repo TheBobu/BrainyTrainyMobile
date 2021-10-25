30aeea922ea7ab2bd8f9d463bd55fdac22ced2c5
Andrei Florescu <andrei.florescu.00@gmail.com>
2021-10-25 19:47:17 +0300

    added spinner on login and register

---
+++ src/components/FullscreenSpinner/index.tsx
@@ -0,0 +1,8 @@
+import { IonSpinner } from "@ionic/react"
+import './spinner.css'
+
+const FullscreenSpinner = () => {
+    return <div className='fullscreen-spinner'> <IonSpinner name="crescent" /></div>
+}
+
+export default FullscreenSpinner;
